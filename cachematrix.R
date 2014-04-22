4fdf2ba7588267b445de36bd73d0b027d3cd3c4f
tuszek <tuszek5@gmail.com>
2014-04-22 02:48:26 +0200

    final ver

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function needs matrix as an argument;
+## returns list of 4 items and getinv() returns NULL
+## so, e.g. c <- rbind(c(1, 2), c(1/3, 1/4))
+##          d <- makeCacheMatrix(c)
 
 makeCacheMatrix <- function(x = matrix()) {
-test
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The output of cacheSolve comes from 
+## cache ("getting cached data" warning is then returned) OR computation.
+## e.g. run: cacheSolve(d) to get inverse of the mtx
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()           
+    if(!is.null(m)) {    # if there is a cache already
+      message("getting cached data") 
+      return(m)          # return the cache without time-consuming computation needed
+    }
+    data <- x$get()      # if there is no cache
+    m <- solve(data, ...)# compute the inverse of the matrix
+    x$setinv(m)          # and store it into x's cache
+    m
 }
+
+## to confirm, run:  d$get() %*% d$getinv() - identity mtx should be returned
+
+## this is the example how <<- op. works - objects are "nested" within functions, not in the global env.
